0a05c77c9c9cdbb374b1f6fd63e17880b08c3e45
Hani_Jandali <hanijandali@gmail.com>
2020-09-03 12:14:04 -0700

    This should commit basic mathematical calculations

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This is the Calculator Project for CS1.0
 hello world
 
 print("This is also a new test change")
+
+
--- Real_Calculator.py
+++ Real_Calculator.py
@@ -1,1 +1,22 @@
-print("Hellow World")
+
+print("Calculator!")
+
+print("Addition")
+number1 = float(input("First number: "))
+number2 = float(input("Second number: "))
+print("Solution: ", number1 + number2)
+
+print("Subtraction")
+print("Solution: ", number1 - number2)
+
+print("Multiplication")
+print("Solution: ", number1 * number2)
+
+print("Division")
+print("Solution: ", number1 / number2)
+
+
+
+
+
+
